58f12b21282c29b46a2de90a58e5f32c605d121a
algarmu <a.garciamu.2018@alumnos.urjc.es>
2022-03-28 20:53:02 +0200

    tienda js fallo

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,3 +1,19 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path')
+
+console.log("Arrancando servidor...")
+
+
+http.createServer((req, res) => {
+    console.log(req.url);
+    if (req.url == '/') {
+        console.log('init');
+
+        fs.readFile('tienda.html', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            console.log("---> Peticion recibida")
+            console.log("Recurso solicitado (URL): " + req.url)
+            res.end(data)
+        })
+    }
+}
