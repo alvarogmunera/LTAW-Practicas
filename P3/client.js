257b1ed4728712aa3af17af6c6deb2f07e53ed43
algarmu <a.garciamu.2018@alumnos.urjc.es>
2022-04-29 18:56:00 +0200

    js p3

--- P3/client.js
+++ P3/client.js
@@ -1,3 +1,8 @@
 //-- Elementos del interfaz
 const display = document.getElementById("display");
 const msg_entry = document.getElementById("msg_entry");
+const send = document.getElementById("send");
+const audio = document.getElementById("audio");
+
+//--Se establece la conexión con el servidor
+const socket = io();
