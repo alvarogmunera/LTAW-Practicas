64d82af7b3b60bad934e504a8dba18f36d40bb17
algarmu <a.garciamu.2018@alumnos.urjc.es>
2022-05-09 19:58:51 +0200

    server js

--- P3/server.js
+++ P3/server.js
@@ -6,3 +6,14 @@ const colors = require('colors');
 const snakeNames = require('snake-names');
 
 const PUERTO = 9000;
+
+//-- Crear una nueva aplciacion web
+const app = express();
+
+//-- Crear un servidor, asosiaco a la App de express
+const server = http.Server(app);
+
+//-- Crear el servidor de websockets, asociado al server htto
+const io = socket(server);
+
+let counter = 0;
