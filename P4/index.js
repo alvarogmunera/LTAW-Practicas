0a42c4057dc7659331260a069b30ff08d10ed3d5
algarmu <a.garciamu.2018@alumnos.urjc.es>
2022-05-20 14:32:57 +0200

    index js p4

--- P4/index.js
+++ P4/index.js
@@ -3,3 +3,24 @@ const qrcode = require('qrcode');
 
 
 console.log("Hola desde el proceso de la web...");
+
+//-- Obtener elementos de la interfaz
+
+const info1 = document.getElementById("info1");
+const info2 = document.getElementById("info2");
+const info3 = document.getElementById("info3");
+const info4 = document.getElementById("info4");
+const info5 = document.getElementById("info5");
+const info6 = document.getElementById("info6");
+
+
+//-- Acceder a la API de node para obtener la info
+//-- Sólo es posible si nos han dado permisos desde
+//-- el proceso princpal
+info1.textContent = process.arch;
+info2.textContent = process.platform;
+info3.textContent = process.cwd();
+
+info4.textContent = process.versions.node;
+info5.textContent = process.versions.chrome;
+info6.textContent = process.versions.electron;
