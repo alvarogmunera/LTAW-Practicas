f1e28faa60a374612bedb74bee3774b1a0b4671a
algarmu <a.garciamu.2018@alumnos.urjc.es>
2022-05-23 20:32:54 +0200

    cambios js

--- P1/tienda.js
+++ P1/tienda.js
@@ -16,60 +16,20 @@ const type = {
   "mp3": "audio/mpeg3"
 };
 
-//-- Creo el servidor
-const server = http.createServer(function(req, res) {
-    //-- Petición recibida
-    console.log('\nPetición recibida');
+const folder = ['CSS', 'Imagenes', 'JS'];
 
-    //-- Se obtiene la URL del recurso
-    let url = new URL(req.url, 'http://' + req.headers['host']);
-    console.log("URL del recurso solicitado: " + url.href);
 
-    //-- Fichero del recurso
-    let recurso = "";
+const server = http.createServer((req, res)=>{
+  console.log("Petición recibida!");
 
-    //-- Analizo recurso, si es raiz --> pag principal
-    if(url.pathname == '/') { 
-      recurso += "/tienda.html" 
-    } else { 
-      recurso = url.pathname;
-    }
+  //-- Valores de la respuesta por defecto
+  let code = 200;
+  let code_msg = "OK";
+  let path = "./front-end";
+  let content_type = "text/html";
+  let folder_exists = false;
+}
 
-    //-- Extensión del recurso
-    extension = recurso.split(".")[1];
-    recurso = '.' + recurso;
-    console.log(' * Recurso solicitado: ' + recurso);
-    console.log(' * Extensión del recurso solicitado: ' + extension); 
-
-    let mime = type_mime[extension];
-    console.log(' * Tipo de MIME: ' + mime);
-
-      //-- Lectura asincrona del fichero
-    fs.readFile(file, (err,data) => {
-
-    //-- Fichero no encontrado, página de error
-    if (err) {
-
-        // Cabecera de error
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        console.log('\n404: Not Found');
-        file = './error.html';
-        data = fs.readFileSync(file);
-        res.write(data);
-        res.end();
-
-    }else{
-      
-        //-- Cabecera de ok
-        res.writeHead(200, {'Content-Type': mime});
-        console.log('\n200: OK');
-
-        //-- Recurso solicitado
-        res.write(data);
-        res.end();
-    }
-  });
-});
 
 //-- Activo el servidor
 server.listen(PUERTO);
